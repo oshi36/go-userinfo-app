a5de87b4cede5539161ef0670bc3fd3412aa9d2d
Oshi Gupta <oshiagupta36@gmail.com>
2023-09-23 00:22:10 +0530

    code files for userinfo

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"htmlgo/server/router"
+	
+	
+)
+
+func main() {
+
+	r := router.Router()
+	
+	fmt.Println("Starting server on the port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
